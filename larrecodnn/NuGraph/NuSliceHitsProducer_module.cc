8f7654fd5b3ca9a9bf55d288e5be4e90d740795d
Giuseppe Cerati <cerati@fnal.gov>
2024-04-24 11:03:06 -0500

    cleaner implementation, also avoid creating empty product when not accessing MC truth

--- larrecodnn/NuGraph/NuSliceHitsProducer_module.cc
+++ larrecodnn/NuGraph/NuSliceHitsProducer_module.cc
@@ -67,7 +67,7 @@ NuSliceHitsProducer::NuSliceHitsProducer(fhicl::ParameterSet const& p)
 {
   // Call appropriate produces<>() functions here.
   produces<std::vector<recob::Hit>>();
-  produces<HitParticleAssociations>();
+  if (!fHitTruthLabel.empty()) produces<HitParticleAssociations>();
 
   // Call appropriate consumes<>() for any products to be retrieved by this module.
 }
@@ -93,10 +93,10 @@ void NuSliceHitsProducer::produce(art::Event& e)
   art::Handle<std::vector<recob::Hit>> hitListHandle;
   e.getByLabel(fHitLabel, hitListHandle);
   std::unique_ptr<art::FindManyP<simb::MCParticle, anab::BackTrackerHitMatchingData>> hittruth;
-  if (fHitTruthLabel != "")
-    hittruth = std::unique_ptr<art::FindManyP<simb::MCParticle, anab::BackTrackerHitMatchingData>>(
-      new art::FindManyP<simb::MCParticle, anab::BackTrackerHitMatchingData>(
-        hitListHandle, e, fHitTruthLabel));
+  if (!fHitTruthLabel.empty()) {
+    hittruth = std::make_unique<art::FindManyP<simb::MCParticle, anab::BackTrackerHitMatchingData>>(
+      hitListHandle, e, fHitTruthLabel);
+  }
 
   for (size_t ipfp = 0; ipfp < inputPfp->size(); ipfp++) {
 
@@ -112,7 +112,7 @@ void NuSliceHitsProducer::produce(art::Event& e)
       auto hit = sliceHits.at(ihit);
       outputHits->emplace_back(*hit);
 
-      if (fHitTruthLabel == "") continue;
+      if (!hittruth) continue;
       std::vector<art::Ptr<simb::MCParticle>> particle_vec = hittruth->at(hit.key());
       std::vector<anab::BackTrackerHitMatchingData const*> match_vec = hittruth->data(hit.key());
       const art::Ptr<recob::Hit> ahp = hitPtrMaker(outputHits->size() - 1);
@@ -123,7 +123,7 @@ void NuSliceHitsProducer::produce(art::Event& e)
   }
 
   e.put(std::move(outputHits));
-  e.put(std::move(outputHitPartAssns));
+  if (!fHitTruthLabel.empty()) e.put(std::move(outputHitPartAssns));
 }
 
 DEFINE_ART_MODULE(NuSliceHitsProducer)
